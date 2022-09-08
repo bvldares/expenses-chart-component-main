ddb7631cd609387717971e44e2cfdc446f56082d
Nunzio Baldares <bvldares@outlook.com>
2022-09-08 23:41:38 +0200

    lesgo

--- script.js
+++ script.js
@@ -1,29 +1,19 @@
 const columns = document.getElementsByClassName("column")
 let values = []
 
-
 fetch("data.json")
 .then(res => res.json())
 .then(data =>{
     data.map(day => values.push(day.amount))
-
-    
     document.querySelector(".statement-expenses").textContent = values.reduce((sum , value) => {
         return sum + value},0).toFixed(2)
-
     for(let i=0; i<columns.length; i++){
-        if(columns[i] > 100){
-            columns[i] = 100  //occhio qui 
+        if(values[i] >= 100){
+            values[i] = 100;
         }
         columns[i].style.height = `${values[i]}%`
     }
-
 })
-
-document.querySelector(".lunedi-value").addEventListener("onmouseover", () =>{
-    
-})
-
 
 
 
